e0eb332126c24b0f2f4d83ef809c619933725ea9
Franzinhashow <valeriacoelhodacunha@gmail.com>
2022-04-04 10:01:21 -0300

    segunda versão

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,9 @@
-var somar = require("./somar");
+var somar = require("./modulos/somar");
+var subtração = require ('./modulos/subtração');
+var multiplicação = require ('./modulos/multiplicação');
+var divisão = require ('./modulos/divisão');
 
 console.log(somar(20,90));
+console.log(subtrair(20,90));
+console.log(multplicar(20,90));
+console.log(dividir(20,90));
